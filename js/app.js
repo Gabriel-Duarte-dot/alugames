c030f7242dddae3cc53fb6eb98e79503f4141c92
Gabriel Duarte <duarteg206@gmail.com>
2024-12-19 19:54:23 -0300

    adicionando confirmação

--- js/app.js
+++ js/app.js
@@ -1,21 +1,22 @@
 
-
-
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    
-    if(imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        // Adiciona uma confirmação antes de devolver o jogo
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
-        botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
     }
 }
-
 
